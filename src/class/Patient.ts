03498f0429a6989f3e098d47c959b14abc96fe09
astrutz <--replace-all>
2022-01-25 20:02:14 +0100

    Remove circular reference in data structure

--- src/class/Patient.ts
+++ src/class/Patient.ts
@@ -1,20 +1,9 @@
-import Appointment from './Appointment';
-import Therapist from './Therpist';
-
 export default class Patient {
   name : string;
 
-  therapist : Therapist;
-
-  appointments : Appointment[];
-
   constructor(
     name : string,
-    therapist : Therapist,
-    appointments : Appointment[],
   ) {
     this.name = name;
-    this.therapist = therapist;
-    this.appointments = appointments;
   }
 }
--- src/class/Therpist.ts
+++ src/class/Therpist.ts
@@ -1,20 +1,9 @@
-import Appointment from './Appointment';
-import Patient from './Patient';
-
 export default class Therapist {
   name: string;
 
-  patients: Patient[];
-
-  appointments: Appointment[];
-
   constructor(
     name : string,
-    patients: Patient[],
-    appointments: Appointment[],
   ) {
     this.name = name;
-    this.patients = patients;
-    this.appointments = appointments;
   }
 }
