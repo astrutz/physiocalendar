2d4c329c570183a7bfbef0690a0cca870b0b3485
astrutz <--replace-all>
2022-05-04 13:05:00 +0200

    Change assets for production

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  assetsDir: process.env.NODE_ENV === 'production' ? '/Users/SAMSUNG/Desktop/Physiokalender/physiocalendar-frontend/dist' : '/',
   transpileDependencies: [
     'vuetify',
   ],
