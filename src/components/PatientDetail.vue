8a6940a77ba4d0f58eb545aa406382c358c46415
Sebastian Mierisch <sebastian.mierisch@gmail.com>
2024-08-06 12:25:34 +0200

    grundlöagen schaffen für backend kommunikation therapeuthen udn patienten

--- src/components/PatientDetail.vue
+++ src/components/PatientDetail.vue
@@ -1,97 +1,78 @@
 <template>
-    <v-card>
-      <v-card-title class="text-h5">Patient bearbeiten</v-card-title>
-      <!-- Formular für die Patientenbearbeitung -->
+  <v-card>
+    <v-card-title class="text-h5">Patient bearbeiten</v-card-title>
     <v-card-text>
       <v-row>
         <v-col>
-          <v-text-field label="ID" :disabled="true" :value="patient.id" v-model="patientInput.id" clearable></v-text-field>
+          <v-text-field label="ID" :disabled="true" v-model="patientInput.id" clearable></v-text-field>
         </v-col>
         <v-col>
-          <v-text-field label="Vorname" :value="patient.firstName" v-model="patientInput.firstName" clearable></v-text-field>
+          <v-text-field label="Vorname" v-model="patientInput.firstName" clearable></v-text-field>
         </v-col>
         <v-col>
-          <v-text-field label="Nachname" :value="patient.name" v-model="patientInput.name" clearable></v-text-field>
+          <v-text-field label="Nachname" v-model="patientInput.lastName" clearable></v-text-field>
         </v-col>
-    </v-row>
-    <v-row>
+      </v-row>
+      <v-row>
         <v-col>
           <v-checkbox label="BWO" v-model="patientInput.isBWO"></v-checkbox>
         </v-col>
         <v-col>
-          <v-text-field
-          label="Aktiv seit"
-          :value="formatDate(patient.activeSince)"
-          v-model="patientInput.activeSince"
-          clearable></v-text-field>
+          <v-text-field label="Aktiv seit" :value="formatDate(patientInput.activeSince)" v-model="patientInput.activeSince" clearable></v-text-field>
         </v-col>
-        <v-col >
-          <v-text-field
-          label="Aktiv bis"
-          :value="formatDate(patient.activeUntil)"
-          v-model="patientInput.activeUntil"
-          clearable></v-text-field>
+        <v-col>
+          <v-text-field label="Aktiv bis" :value="formatDate(patientInput.activeUntil)" v-model="patientInput.activeUntil" clearable></v-text-field>
         </v-col>
-    </v-row>
-    <v-spacer></v-spacer>
-    <v-row>
-      <h6 class="text-h6">Termine für {{ patient.name + ', ' + patient.firstName}}</h6>
-    </v-row>
-    <v-row>
-      <v-list>
-        <v-list-item v-for="(appointment, index) in appointments" :key="index">
-          <v-list-item-title>{{ appointment.startTime }} - {{ appointment.endTime }}</v-list-item-title>
-          <!-- Hier weitere Informationen zu jedem Termin anzeigen -->
-        </v-list-item>
-      </v-list>
-    </v-row>
+      </v-row>
+      <v-row>
+        <h6 class="text-h6">Termine für {{ patientInput.lastName + ', ' + patientInput.firstName}}</h6>
+      </v-row>
+      <v-row>
+        <v-list>
+          <v-list-item v-for="(appointment, index) in appointments" :key="index">
+            <v-list-item-title>{{ formatDate(appointment.startTime) }} - {{ formatDate(appointment.endTime) }}</v-list-item-title>
+            <!-- Weitere Termininformationen anzeigen -->
+          </v-list-item>
+        </v-list>
+      </v-row>
     </v-card-text>
-      <!-- Dialog-Steuerung -->
-      <v-card-actions>
-        <v-btn @click="cancelChanges">Abbrechen</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="error" @click="deletePatient">Patient Löschen</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" @click="saveChanges">Speichern</v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
+    <v-card-actions>
+      <v-btn @click="cancelChanges">Abbrechen</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="error" @click="deletePatient">Patient löschen</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" @click="saveChanges">Speichern</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import Appointment from '@/class/Appointment';
 import Patient from '@/class/Patient';
+import Appointment from '@/class/Appointment';
 
 @Component
 export default class PatientDetail extends Vue {
   @Prop({ required: true }) patient!: Patient;
-
   @Prop({ required: true }) appointments!: Appointment[];
 
   patientInput: Patient = { ...this.patient };
 
-  // Methode zum Schließen des Dialogs und Abbrechen der Bearbeitung
   cancelChanges(): void {
     this.$emit('cancel');
   }
 
-  // Methode zum Schließen des Dialogs und Speichern der Änderungen
   saveChanges(): void {
-    // Hier kannst du die Logik zum Speichern der Änderungen implementieren
-    this.$emit('save');
+    this.$emit('save', { patient: this.patientInput });
   }
 
-  // Methode zum Schließen des Dialogs und Speichern der Änderungen
   deletePatient(): void {
-    // Hier kannst du die Logik zum Speichern der Änderungen implementieren
     this.$emit('deletePatient', { patient: this.patientInput });
   }
 
-  // eslint-disable-next-line class-methods-use-this
   formatDate(date: Date | undefined): string {
-    if (!date) return ''; // Sicherstellen, dass ein gültiges Date-Objekt vorhanden ist
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return new Date(date).toLocaleDateString('de-DE' as string, options); // Typumwandlung zu string
+    if (!date) return '';
+    return new Date(date).toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit' });
   }
 }
 </script>
--- src/components/Patients.vue
+++ src/components/Patients.vue
@@ -3,18 +3,18 @@
     <v-card-text class="pt-5">
       <v-row>
         <v-text-field v-model="search" label="Suche" clearable @input="filterPatients"/>
-        <v-btn color="green" @click="openCreatePatientDialog"> <v-icon color="white">mdi-plus</v-icon></v-btn>
+        <v-btn color="green" @click="openCreatePatientDialog">
+          <v-icon color="white">mdi-plus</v-icon>
+        </v-btn>
       </v-row>
     </v-card-text>
 
-    <!-- Tabelle mit Patienten -->
     <v-data-table
       :headers="headers"
       :items="filteredPatients"
       item-key="id"
       @click:row="showDetail"
     >
-      <!-- Formatieren der Datums- und isBWO-Spalten -->
       <template v-slot:item.activeSince="{ item }">
         {{ formatDate(item.activeSince) }}
       </template>
@@ -26,14 +26,12 @@
       </template>
     </v-data-table>
 
-    <!-- Dialog für die Patientenerstellung -->
     <v-dialog v-model="createPatientDialog" max-width="600">
       <v-card>
         <CreatePatient @save="createPatient($event)" @cancel="closeCreatePatientDialog" />
       </v-card>
     </v-dialog>
 
-    <!-- Dialog für die Patientendetails -->
     <v-dialog v-model="detailDialog" max-width="600">
       <v-card>
         <PatientDetail
@@ -51,11 +49,10 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import PatientDetail from '@/components/PatientDetail.vue';
+import CreatePatient from '@/components/CreatePatient.vue';
+import PatientService from '@/services/PatientService';
 import Patient from '@/class/Patient';
 import Appointment from '@/class/Appointment';
-import { getModule } from 'vuex-module-decorators';
-import Store from '../store/backup';
-import CreatePatient from './CreatePatient.vue';
 
 @Component({
   components: {
@@ -65,116 +62,85 @@ import CreatePatient from './CreatePatient.vue';
 })
 
 export default class Patients extends Vue {
-  // Tabellenheader
   headers = [
     { text: 'Vorname', value: 'firstName' },
-    { text: 'Nachname', value: 'name' },
+    { text: 'Nachname', value: 'lastName' },
     { text: 'Aktiv seit', value: 'activeSince' },
     { text: 'Aktiv bis', value: 'activeUntil' },
     { text: 'BWO', value: 'isBWO' },
   ];
 
-  store = getModule(Store);
-
   detailDialog = false;
-
   createPatientDialog = false;
-
   selectedPatient: Patient | null = null;
-
-  selectedPatientId: string | null = null;
-
   selectedPatientAppointments: Appointment[] = [];
-
   patients: Patient[] = [];
-
   filteredPatients: Patient[] = [];
-
   search = '';
 
   async created(): Promise<void> {
-    // Warten, bis die Patientendaten geladen sind
     await this.loadPatients();
   }
 
   async loadPatients(): Promise<void> {
-    this.patients = await this.store.getPatients();
-    console.log(this.patients);
+    this.patients = await PatientService.getAllPatients();
     this.filteredPatients = [...this.patients];
   }
 
   async showDetail(patient: Patient): Promise<void> {
-    this.selectedPatient = patient; // setze den ausgewählten Patienten
-    this.selectedPatientId = patient.id; // setze die ausgewählte Patienten-ID
-    await this.loadPatientDetails(patient); // Asynchrone Funktion aufrufen
-    this.detailDialog = true; // setze detailDialog auf true, um den Dialog anzuzeigen
+    this.selectedPatient = patient;
+    this.selectedPatientAppointments = await PatientService.getPatientAppointments(patient.id); // Wenn verfügbar
+    this.detailDialog = true;
   }
 
-  async loadPatientDetails(patient: Patient): Promise<void> {
-    // Annahme: Funktion zum Abrufen von Terminen für einen Patienten
-    await this.getAppointmentsForPatient(patient);
+  async savePatientChanges(event: { patient: Patient }): Promise<void> {
+    if (event.patient) {
+      await PatientService.updatePatient(event.patient.id, event.patient);
+      this.loadPatients();
+      this.closeDetailDialog();
+    }
   }
 
-  getAppointmentsForPatient(patient: Patient): void {
-    const exampleAppointments: Appointment[] = this.store.getAppointmentsForPatient(patient);
-    this.selectedPatientAppointments = exampleAppointments;
+  async deletePatient(event: { patient: Patient }): Promise<void> {
+    if (event.patient) {
+      await PatientService.deletePatient(event.patient.id);
+      this.loadPatients();
+      this.closeDetailDialog();
+    }
   }
 
-  // Methode zum Filtern der Patientenliste basierend auf dem Suchbegriff
-  filterPatients(): void {
-    const searchTerm = this.search.toLowerCase();
-    // eslint-disable-next-line
-    this.filteredPatients = this.patients.filter((patient) => {
-      return patient.firstName.toLowerCase().includes(searchTerm) || patient.name.toLowerCase().includes(searchTerm);
-    });
+  async createPatient(event: { patient: Patient }): Promise<void> {
+    if (event.patient) {
+      await PatientService.createPatient(event.patient);
+      this.loadPatients();
+      this.closeCreatePatientDialog();
+    }
   }
 
-  // Methode zum Schließen der Detailansicht
   closeDetailDialog(): void {
     this.detailDialog = false;
     this.selectedPatient = null;
     this.selectedPatientAppointments = [];
   }
 
-  // Methode zum Speichern der Änderungen des Patienten
-  savePatientChanges(): void {
-    // Hier die Logik zum Speichern der Patientenänderungen implementieren
-    this.closeDetailDialog();
-  }
-
-  // Methode zum Speichern der Änderungen des Patienten
-  deletePatient(event: {
-      patient: Patient,
-    }): void {
-    this.store.deletePatient(event.patient);
-    this.closeDetailDialog();
-    this.loadPatients();
-  }
-
-  // Methode zum Speichern der Änderungen des Patienten
-  createPatient(event: {
-      patient: Patient,
-    }): void {
-    this.store.addPatient(event.patient);
-    this.closeCreatePatientDialog();
-    this.loadPatients();
-  }
-
-  // Methode zum Speichern der Änderungen des Patienten
   closeCreatePatientDialog(): void {
     this.createPatientDialog = false;
   }
 
-  // Methode zum Speichern der Änderungen des Patienten
   openCreatePatientDialog(): void {
     this.createPatientDialog = true;
   }
 
-  // eslint-disable-next-line class-methods-use-this
+  filterPatients(): void {
+    const searchTerm = this.search.toLowerCase();
+    this.filteredPatients = this.patients.filter(
+      patient => patient.firstName.toLowerCase().includes(searchTerm) || patient.lastName.toLowerCase().includes(searchTerm)
+    );
+  }
+
   formatDate(date: Date | undefined): string {
-    if (!date) return ''; // Sicherstellen, dass ein gültiges Date-Objekt vorhanden ist
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return new Date(date).toLocaleDateString('de-DE' as string, options); // Typumwandlung zu string
+    if (!date) return '';
+    return new Date(date).toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit' });
   }
 }
 </script>
